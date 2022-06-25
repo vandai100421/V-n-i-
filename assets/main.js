77a58f892e928caae65268b413ec59b5ebef365a
VanDai <vandai1042001@gmail.com>
2022-06-25 10:24:15 +0700

    add text to index.html

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+// ReactDOM
+//     .createRoot(document.getElementById('root'))
+//     .render(<h1>Hello, world!</h1>);
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+    );
